7f589fd172422dfd82296a6bc0e79a3465dbe223
KarenLteif <108737797+KarenLteif@users.noreply.github.com>
2023-02-06 23:18:15 +0200

    Add files via upload

---
+++ TestingServlet/src/main/java/HelloServlet.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns={"/myservlet"})
+public class HelloServlet extends HttpServlet {
+    public void service( HttpServletRequest request,HttpServletResponse response )
+            throws ServletException, IOException {
+        response.setContentType( "text/html" );
+        PrintWriter out = response.getWriter();
+        out.println( "<html><body>" );
+        out.println( "<h1>Hello Karen Lteif</h1>" );
+        out.println( "</body></html>" );
+    }
+}
---
+++ TestingServlet/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Veuillez cliquer sur le lien pour acceder a la page voulue </h2>
+<a href="http://localhost:8080/TestingServlet/myservlet">Lien vers le Servlet</a>
+</body>
+</html>
